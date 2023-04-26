ccb0a77bb9c3c561a97c416b7d144fbb67f0f151
VysotskaNatali <Bljondinka7@gmail.com>
2023-04-27 02:03:34 +0300

    Add style component and google maps module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,7 +25,7 @@ import { FooterComponent } from './component/footer/footer.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/contact/contact.module.ts
+++ src/app/pages/contact/contact.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
 import { SharedModule } from '../../shared/shared.module';
+import { GoogleMapsModule } from '@angular/google-maps';
+
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [CommonModule, SharedModule, ContactRoutingModule],
+  imports: [CommonModule, 
+    SharedModule, 
+    ContactRoutingModule,
+    GoogleMapsModule,
+  ],
 })
 export class ContactModule {}
